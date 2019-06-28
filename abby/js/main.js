c2e7976bd404f76420e0dfa0c6ca976016dc29c2
abby2212 <52089035+abby2212@users.noreply.github.com>
2019-06-28 14:02:29 +0100

    Add files via upload

---
+++ abby/js/main.js
@@ -0,0 +1,43 @@
+document.getElementById("btw1").addEventListener("click",function(){
+    
+    var x = document.getElementById("TA").style.fontWeight;
+    if (x=="normal")
+    document.getElementById("TA").style.fontWeight = "bold";
+else
+document.getElementById("TA").style.fontWeight="normal";})
+
+
+
+document.getElementById("btw2").addEventListener("click",function(){
+    
+    var x = document.getElementById("TA").style.fontStyle;
+    if (x=="normal")
+    document.getElementById("TA").style.fontStyle="italic";
+else
+document.getElementById("TA").style.fontStyle="normal";})
+
+
+
+
+function souligne(){
+    x=document.forms[0].A.style.textDecoration;
+    if (x=="none")
+    document.forms[0].A.style.textDecoration="underline";
+    else
+    document.forms[0].A.style.textDecoration="none";
+    console.log(x);
+}
+
+function taille()
+{y=document.getElementById("myselect");
+x=y.options[y.selectedIndex].text;
+document.getElementById("TA").style.fontSize=x;
+}
+function tt(){
+   var ahmed=document.getElementById("myselectA");
+   var x=ahmed.options[ahmed.selectedIndex].text;
+   document.getElementById("TA").style.fontFamily=x;
+}
+
+
+
